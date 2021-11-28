2938c13844be4871e76dc395e7de7cdc3ead513f
tomato-commits <arthurpeter7@gmail.com>
2021-11-28 23:13:35 +0200

    add simple light box

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,5 +1,7 @@
 import { fetchImages } from "./fetchImages";
 import { Notify } from "notiflix";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const searchForm = document.getElementById("search-form");
 const gallery = document.querySelector(".gallery");
@@ -70,28 +72,35 @@ async function loadImages(value, page = 1) {
 
 function renderGallery(images) {
     const markup = images
-        .map(({ webformatURL, tags, likes, views, comments, downloads }) => {
+        .map(({ webformatURL, largeImageURL , tags, likes, views, comments, downloads }) => {
             return `<div class="photo-card">
-                <div><img src="${webformatURL}" alt="${tags}" loading="lazy" /></div>
-                <div class="info">
-                    <p class="info-item">
-                    <b>Likes</b>${likes}
-                    </p>
-                    <p class="info-item">
-                    <b>Views</b>${views}
-                    </p>
-                    <p class="info-item">
-                    <b>Comments</b>${comments}
-                    </p>
-                    <p class="info-item">
-                    <b>Downloads</b>${downloads}
-                    </p>
-                </div>
+                <a href="${largeImageURL}">
+                    <div><img src="${webformatURL}" alt="${tags}" loading="lazy" /></div>
+                    <div class="info">
+                        <p class="info-item">
+                        <b>Likes</b>${likes}
+                        </p>
+                        <p class="info-item">
+                        <b>Views</b>${views}
+                        </p>
+                        <p class="info-item">
+                        <b>Comments</b>${comments}
+                        </p>
+                        <p class="info-item">
+                        <b>Downloads</b>${downloads}
+                        </p>
+                    </div>
+                </a>
             </div>`;
             })
         .join("");
     
     gallery.innerHTML += markup;
+
+    const lightbox = new SimpleLightbox('.gallery a', {
+        captionDelay: 250,
+        captionsData: 'alt',
+    });
 }
 
 function clearMarkup() {
