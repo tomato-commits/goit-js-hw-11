9567bce9eb82aa811048f4475edda40d86a54204
tomato-commits <arthurpeter7@gmail.com>
2021-11-28 21:48:13 +0200

    add axios

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 const KEY = "24561265-9956eb3917e109e6a295ac624";
 
 async function fetchImages({ value, page }) {
@@ -11,10 +12,8 @@ async function fetchImages({ value, page }) {
             page: page || 1,
             per_page: 40,
         });
-        const response = await fetch(`https://pixabay.com/api/?${params}`);
-        const images = await response.json();
-
-        console.log(images);
+        const response = await axios.get(`https://pixabay.com/api/?${params}`);
+        const images = response.data;
 
         return images;
     } catch (error) {
